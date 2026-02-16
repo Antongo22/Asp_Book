6f7386ee5d463cf75366a7b3318240f8b6a176df
Antongo22 <80984465+Antongo22@users.noreply.github.com>
2026-02-16 16:50:41 +0300

    доработаны главы 5-12

--- Asp_Book.Chapter05/Pages/Index.cshtml.cs
+++ Asp_Book.Chapter05/Pages/Index.cshtml.cs
@@ -1,10 +1,31 @@
+using Asp_Book.Chapter05.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Asp_Book.Chapter05.Pages;
 
 public class IndexModel : PageModel
 {
+    [BindProperty]
+    public ContactFormModel DemoForm { get; set; } = new();
+
+    public string? SuccessMessage { get; set; }
+
     public void OnGet()
     {
+    }
+
+    public IActionResult OnPost()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        SuccessMessage = $"Спасибо, {DemoForm.Name}! Ваше сообщение получено.";
+        DemoForm = new ContactFormModel();
+        ModelState.Clear();
+        
+        return Page();
     }
 }
--- Asp_Book.Chapter11/Program.cs
+++ Asp_Book.Chapter11/Program.cs
@@ -45,7 +45,7 @@ builder.Services.AddAuthentication(options =>
         {
             var accessToken = context.Request.Query["access_token"];
             var path = context.HttpContext.Request.Path;
-            if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/chathub"))
+            if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/authorizedchathub"))
             {
                 context.Token = accessToken;
             }
